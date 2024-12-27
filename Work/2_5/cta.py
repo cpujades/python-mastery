8104416fd03cf5f38f550fdb89108e0a4f43aa3b
Carlos Pujades <cpcarlospujades@gmail.com>
2024-12-27 12:45:26 +0100

    Completed exercise 2_5

---
+++ Work/2_5/cta.py
@@ -0,0 +1,70 @@
+# cta.py
+
+# exercise 2.5
+
+import readrides
+from collections import Counter
+
+
+def total_rides(rides):
+    """
+    Calculate the total number of rides
+    """
+    tot_rides = {ride["route"] for ride in rides}
+    return len(tot_rides)
+
+
+def people_on_date_and_route(date, route):
+    """
+    Calculate the total number of people on a given date
+    """
+    tot_rides = 0
+    for ride in rides:
+        if ride["date"] == date and ride["route"] == route:
+            tot_rides += ride["rides"]
+    return tot_rides
+
+
+def total_number_of_rides(rides):
+    """
+    Calculate the total number of rides
+    """
+    total = Counter()
+    for ride in rides:
+        total[ride["route"]] += ride["rides"]
+
+    top_three = total.most_common(3)
+    return top_three
+
+
+def ride_increase(rides):
+    """
+    Calculate the ride increase
+    """
+    counter_2001 = Counter()
+    counter_2011 = Counter()
+    for i in range(2001, 2012):
+        if i == 2001 or i == 2011:
+            for ride in rides:
+                if str(i) in ride["date"]:
+                    if i == 2001:
+                        counter_2001[ride["route"]] += ride["rides"]
+                    else:
+                        counter_2011[ride["route"]] += ride["rides"]
+
+            ride_increase = counter_2011 - counter_2001
+            top_five = ride_increase.most_common(5)
+        else:
+            continue
+    return top_five
+
+
+if __name__ == "__main__":
+    rides = readrides.read_rides_as_dicts("../Data/ctabus.csv")
+    print("Nº of bus routes in Chicago:", total_rides(rides))
+    print(
+        "Nº of people on 02/02/2011 on route 22:",
+        people_on_date_and_route("11/25/2003", "22"),
+    )
+    print("Top 3 bus routes with most rides:", total_number_of_rides(rides))
+    print("Top 5 bus routes with most rides increase:", ride_increase(rides))
---
+++ Work/2_5/readrides.py
@@ -0,0 +1,136 @@
+# readrides.py
+
+# exercise 2.5
+
+
+import csv
+import _collections_abc as collections
+from typing import overload
+
+
+class RideData(collections.Sequence):
+    def __init__(self):
+        self.routes = []
+        self.dates = []
+        self.daytypes = []
+        self.numrides = []
+
+    def __len__(self):
+        return len(self.routes)
+
+    def __getitem__(self, idx):
+        if isinstance(idx, slice):
+            data = RideData()
+            data.routes = self.routes[idx]
+            data.dates = self.dates[idx]
+            data.daytypes = self.daytypes[idx]
+            data.numrides = self.numrides[idx]
+            return data
+        return {
+            "route": self.routes[idx],
+            "date": self.dates[idx],
+            "daytype": self.daytypes[idx],
+            "rides": self.numrides[idx],
+        }
+
+    def append(self, d):
+        self.routes.append(d["route"])
+        self.dates.append(d["date"])
+        self.daytypes.append(d["daytype"])
+        self.numrides.append(d["rides"])
+
+
+def read_rides_as_tuples(filename):
+    """
+    Read the bus ride data as a list of tuples
+    """
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)  # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = (route, date, daytype, rides)
+            records.append(record)
+    return records
+
+
+def read_rides_as_dicts(filename):
+    """
+    Read the bus ride data as a list of dictionaries
+    """
+    records = RideData()
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)  # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = {
+                "route": route,
+                "date": date,
+                "daytype": daytype,
+                "rides": rides,
+            }
+            records.append(record)
+    return records
+
+
+class Row:
+    __slots__ = ["route", "date", "daytype", "rides"]
+
+    def __init__(self, route, date, daytype, rides):
+        self.route = route
+        self.date = date
+        self.daytype = daytype
+        self.rides = rides
+
+
+def read_rides_as_instances(filename):
+    """
+    Read the bus ride data as a list of instances
+    """
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)  # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = Row(route, date, daytype, rides)
+            records.append(record)
+    return records
+
+
+def read_rides_as_columns(filename):
+    """
+    Read the bus ride data as a list of columns
+    """
+    routes = []
+    dates = []
+    daytypes = []
+    numrides = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)  # Skip headers
+        for row in rows:
+            routes.append(row[0])
+            dates.append(row[1])
+            daytypes.append(row[2])
+            numrides.append(int(row[3]))
+    return dict(route=routes, date=dates, daytype=daytypes, rides=numrides)
+
+
+if __name__ == "__main__":
+    import tracemalloc
+
+    tracemalloc.start()
+    rows = read_rides_as_dicts("../Data/ctabus.csv")
+    print("Memory Use: Current %d, Peak %d" % tracemalloc.get_traced_memory())
