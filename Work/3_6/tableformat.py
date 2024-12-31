867aadeef3962dc8333910b555a06ca1d21ad8b6
Carlos Pujades <cpcarlospujades@gmail.com>
2024-12-31 12:09:26 +0100

    Completed exercise 3_6

---
+++ Work/3_6/tableformat.py
@@ -0,0 +1,61 @@
+# tableformat.py
+
+# exercise 3.2
+
+
+class TableFormatter:
+    def headings(self, headers):
+        raise NotImplementedError
+
+    def row(self, rowdata):
+        raise NotImplementedError
+
+
+class TextTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print(" ".join(f"{h:>10s}" for h in headers))
+        print(("-" * 10 + " ") * len(headers))
+
+    def row(self, rowdata):
+        rowdata = [str(d) for d in rowdata]
+        print(" ".join(f"{d:>10s}" for d in rowdata))
+
+
+class CSVTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print(",".join(headers))
+
+    def row(self, rowdata):
+        print(",".join(rowdata))
+
+
+class HTMLTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print("<tr>", end="")
+        for h in headers:
+            print(f"<th>{h}</th>", end="")
+        print("</tr>")
+
+    def row(self, rowdata):
+        print("<tr>", end="")
+        for d in rowdata:
+            print(f"<td>{d}</td>", end="")
+        print("</tr>")
+
+
+def create_formatter(name):
+    if name == "text":
+        return TextTableFormatter()
+    elif name == "csv":
+        return CSVTableFormatter()
+    elif name == "html":
+        return HTMLTableFormatter()
+    else:
+        raise ValueError(f"Unknown format {name}")
+
+
+def print_table(records, attributes, formatter):
+    formatter.headings(attributes)
+    for r in records:
+        rowdata = [getattr(r, attr) for attr in attributes]
+        formatter.row(rowdata)
